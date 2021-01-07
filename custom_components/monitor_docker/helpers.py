90608d00e6875fd0870662375ebb7b6719493b77
Reinout Segers <rsegers@reinout.us>
2021-01-07 22:49:43 +0100

    Add health attribute

--- custom_components/monitor_docker/helpers.py
+++ custom_components/monitor_docker/helpers.py
@@ -42,6 +42,7 @@ from .const import (
     CONTAINER_STATS_NETWORK_TOTAL_DOWN,
     DOCKER_INFO_IMAGES,
     CONTAINER_INFO_STATE,
+    CONTAINER_INFO_HEALTH,
     CONTAINER_INFO_STATUS,
     CONTAINER_INFO_UPTIME,
     DOCKER_INFO_CONTAINER_RUNNING,
@@ -588,8 +589,9 @@ class DockerContainerAPI:
 
         raw = await self._container.show()
 
-        self._info[CONTAINER_INFO_STATE] = raw["State"]["Status"]
-        self._info[CONTAINER_INFO_IMAGE] = raw["Config"]["Image"]
+        self._info[CONTAINER_INFO_STATE]  = raw["State"]["Status"]
+        self._info[CONTAINER_INFO_HEALTH] = raw["State"]["Health"]["Status"]
+        self._info[CONTAINER_INFO_IMAGE]  = raw["Config"]["Image"]
         self._info[CONTAINER_INFO_NETWORK_AVAILABLE] = (
             False if raw["HostConfig"]["NetworkMode"] in ["host", "none"] else True
         )
